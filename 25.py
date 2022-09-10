b92e4465ebc88d6bf20a0e0a6e4b1efeceaefe80
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-09-10 10:03:11 +1000

    .

--- 25.py
+++ 25.py
@@ -10,13 +10,13 @@ for star in starValues:
             number = int('73'+star+'5'+str(f1)+'486'+str(f2))
             if number<=10**9 and number%43==0:
                 numbers.append(number)
+numbers = list(set(numbers))
 numbers.sort()
 for i in numbers:
     print(i,i//43)    
 
 # 73584868 1711276
 # 734514863 17081741
-# 735554861 17105927
 # 735554861 17105927
 # 735894862 17113834
 # 737524863 17151741
